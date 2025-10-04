50cbc41aa744027051788ab0448dee932aeb4be9
0715yk <yongkiniiii@gmail.com>
2025-10-04 14:23:10 +0900

    반응형 UI 수정

--- components/organisms/header.tsx
+++ components/organisms/header.tsx
@@ -62,7 +62,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center px-4">
+      <div className="container flex h-14 w-full max-w-full items-center justify-between px-4">
         <div className="mr-4 flex">
           <button
             onClick={() => router.push("/")}
